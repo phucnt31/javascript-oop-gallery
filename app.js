d4437f15c2a931d4f87a1db97ec86b8d9e2db905
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-25 16:21:02 +0700

    Close modal function finished

--- app.js
+++ app.js
@@ -24,6 +24,9 @@ function Gallery(element) {
 
   // bind functions
   // this.openModal = this.openModal.bind(this);
+  this.closeModal = this.closeModal.bind(this);
+  this.nextImage = this.nextImage.bind(this);
+  this.prevImage = this.prevImage.bind(this);
   // add event
   this.container.addEventListener(
     "click",
@@ -44,12 +47,25 @@ Gallery.prototype.openModal = function (selectedImage, list) {
     })
     .join("");
   this.modal.classList.add("open");
+  this.closeBtn.addEventListener("click", this.closeModal);
+  this.nextBtn.addEventListener("click", this.nextImage);
+  this.prevBtn.addEventListener("click", this.prevImage);
 };
 
 Gallery.prototype.setMainImage = function (selectedImage) {
   this.modalImg.src = selectedImage.src;
   this.imageName.textContent = selectedImage.title;
 };
+
+Gallery.prototype.closeModal = function () {
+  this.modal.classList.remove("open");
+  this.closeBtn.removeEventListener("click", this.closeModal);
+  this.nextBtn.removeEventListener("click", this.nextImage);
+  this.prevBtn.removeEventListener("click", this.prevImage);
+};
+
+Gallery.prototype.nextImage = function () {};
+Gallery.prototype.prevImage = function () {};
 
 const nature = new Gallery(getElement(".nature"));
 const city = new Gallery(getElement(".city"));
