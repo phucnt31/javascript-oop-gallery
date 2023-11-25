ce2d7d89a916e734adf774caec1ded4a9a9f7614
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-25 12:39:13 +0700

    Open modal function setup

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ function getElement(selection) {
 }
 
 function Gallery(element) {
+  this.container = element;
   this.list = [...element.querySelectorAll(".img")];
   // target
   this.modal = getElement(".modal");
@@ -17,7 +18,25 @@ function Gallery(element) {
   this.closeBtn = getElement(".close-btn");
   this.nextBtn = getElement(".next-btn");
   this.prevBtn = getElement(".prev-btn");
+  // self ref
+  // let self = this;
+
+  // bind functions
+  // this.openModal = this.openModal.bind(this);
+  // add event
+  this.container.addEventListener(
+    "click",
+    function (e) {
+      console.log(this);
+      this.openModal();
+    }.bind(this)
+  );
 }
+
+Gallery.prototype.openModal = function () {
+  // console.log(this);
+  this.modal.classList.add("open");
+};
 
 const nature = new Gallery(getElement(".nature"));
 const city = new Gallery(getElement(".city"));
